b4ed14bb5390535e8eb0712ab167f3133c64337d
DmitryKartamyshev <dima_kartamyshev@ukr.net>
2019-08-06 23:25:47 +0300

    create main layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Real Property</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><router-link class="nav-link" to="/">Home</router-link></li>
+            <li class="nav-item" ><router-link class="nav-link" to="/about">About</router-link></li>
+          </ul>
+        </div>
+      </nav>
     </div>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mt-5 container text-center justify-content-center align-items-center">
+      <img class="mb-4" src="https://skyup.studio/wp-content/uploads/2019/02/logo.png" alt="">
+       <div class="m-auto d-flex col-6 justify-content-center align-items-center flex-column">
+           <input type="text" class="form-control" placeholder="Search">
+       </div>
+      <div class="checkbox mb-3">
+          <label>
+              <input type="checkbox" value="remember-me"> Remember me
+          </label>
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
