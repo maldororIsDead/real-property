e5462a165d5f62e9aa0fa4e61e7b3db84789f7c5
DmitryKartamyshev <dima_kartamyshev@ukr.net>
2019-08-07 02:01:04 +0300

    remove about component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,16 +2,10 @@
   <div id="app">
     <div id="nav">
       <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Real Property</a>
+        <router-link class="navbar-brand" to="/">Real Property</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item"><router-link class="nav-link" to="/">Home</router-link></li>
-            <li class="nav-item" ><router-link class="nav-link" to="/about">About</router-link></li>
-          </ul>
-        </div>
       </nav>
     </div>
     <router-view/>
